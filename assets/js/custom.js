1541018d21e124ced1333bf3e5c6cd2b1c45793d
natsaros <natsaros@hotmail.com>
2016-02-04 20:18:41 +0200

    contact page basic layout

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     });
 
     // Add smooth scrolling to all links in navbar + footer link
-    $(document).on('click','footer a.toTop', function (event) {
+    $(document).on('click', 'footer a.toTop', function (event) {
         // Prevent default anchor click behavior
         event.preventDefault();
 
@@ -23,9 +23,39 @@ $(document).ready(function () {
             //window.location.hash = hash;
         });
     });
-
+    if ($('body').attr('id') == 'contact') {
+        mapInit();
+    }
 
 });
+
+$(window).load(function () {
+
+    $('footer .toTop').attr('href', '#' + $('body').attr('id'));
+});
+
+function initialize(myCenter) {
+    var mapProp = {
+        center: myCenter,
+        zoom: 16,
+        scrollwheel: false,
+        draggable: true,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+    var marker = new google.maps.Marker({
+        position: myCenter,
+    });
+
+    marker.setMap(map);
+}
+
+function mapInit() {
+    var myCenter = new google.maps.LatLng(37.884851, 23.746829);
+    google.maps.event.addDomListener(window, 'load', initialize(myCenter));
+}
 
 $(window).scroll(function () {
     $(".slideanim").each(function () {
