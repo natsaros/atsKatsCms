ccd46234806e34a812d250c6514ec079fb060a61
natsaros <natsaros@hotmail.com>
2016-04-12 00:00:51 +0300

    added ios related meta

--- php/header.php
+++ php/header.php
@@ -2,7 +2,9 @@
     <title>Fitness House</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="mobile-web-app-capable" content="yes">
     <meta name="description" content="Fitness house">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta name="keywords"
           content="pilates,aerial,yoga,fitness house,fitness,elliniko,glyfada,peny,kasfiki,personal trainer">
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,300italic,300,100italic&subset=latin,greek,greek-ext'
@@ -10,12 +12,13 @@
 
 
     <link rel="icon" type="image/png" href="<?php echo $REQUEST_URI ?>assets/img/favicon.png"/>
+    <link rel="icon" sizes="192x192" href="<?php echo $REQUEST_URI ?>assets/img/favicon.png">
 
     <link rel="apple-touch-icon" href="<?php echo $REQUEST_URI ?>/assets/ios/Icon.png" />
-    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo $REQUEST_URI ?>/assets/ios/Icon-Small-50.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo $REQUEST_URI ?>/assets/ios/Icon-72.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo $REQUEST_URI ?>/assets/ios/Icon@2x.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo $REQUEST_URI ?>/assets/ios/Icon-72@2x.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo $REQUEST_URI ?>assets/ios/Icon-Small-50.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo $REQUEST_URI ?>assets/ios/Icon-72.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo $REQUEST_URI ?>assets/ios/Icon@2x.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo $REQUEST_URI ?>assets/ios/Icon-72@2x.png" />
 
     <link rel="stylesheet" href="<?php echo $REQUEST_URI ?>assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo $REQUEST_URI ?>assets/css/custom.css">
