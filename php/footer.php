07726f35799969b3aa0f634f2d44417775f3bafd
natsaros <natsaros@hotmail.com>
2016-04-11 21:52:29 +0300

    pretty urls

--- php/footer.php
+++ php/footer.php
@@ -3,11 +3,11 @@
         <span class="glyphicon glyphicon-chevron-up"></span>
     </a>
     <ul>
-        <li><a href="<?php echo $REQUEST_URI ?>index.php">Αρχική</a></li>
-        <li><a href="<?php echo $REQUEST_URI ?>index.php?id=about">Το Studio</a></li>
-        <li><a href="<?php echo $REQUEST_URI ?>index.php?id=program">Πρόγραμμα</a></li>
+        <li><a href="<?php echo $REQUEST_URI ?>home">Αρχική</a></li>
+        <li><a href="<?php echo $REQUEST_URI ?>about">Το Studio</a></li>
+        <li><a href="<?php echo $REQUEST_URI ?>program">Πρόγραμμα</a></li>
         <!--<li><a href="#blog">Blog</a></li>-->
-        <li><a href="<?php echo $REQUEST_URI ?>index.php?id=contact">Επικοινωνία</a></li>
+        <li><a href="<?php echo $REQUEST_URI ?>contact">Επικοινωνία</a></li>
         <!--<li><a href="#bookNow">Book Now</a></li>-->
     </ul>
     <div class="extraInfo">&copy; 2015 Copyright</div>
--- php/menu.php
+++ php/menu.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
-            <a class="navbar-brand" href="<?php $REQUEST_URI ?>index.php?id=home">
+            <a class="navbar-brand" href="<?php $REQUEST_URI ?>home">
                 <img src="<?php $REQUEST_URI ?>assets/img/logo.png" alt="Fitness House" class="img-responsive">
             </a>
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -13,11 +13,11 @@
 
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav navbar-right">
-                <li class="<?php if($pageId == "home"){ ?>active<?php } ?>"><a href="<?php $REQUEST_URI ?>index.php?id=home">Αρχική</a></li>
-                <li class="<?php if($pageId == "about"){ ?>active<?php } ?>"><a href="<?php $REQUEST_URI ?>index.php?id=about">Το Studio</a></li>
-                <li class="<?php if($pageId == "program"){ ?>active<?php } ?>"><a href="<?php $REQUEST_URI ?>index.php?id=program">Πρόγραμμα</a></li>
+                <li class="<?php if($pageId == "home"){ ?>active<?php } ?>"><a href="<?php $REQUEST_URI ?>home">Αρχική</a></li>
+                <li class="<?php if($pageId == "about"){ ?>active<?php } ?>"><a href="<?php $REQUEST_URI ?>about">Το Studio</a></li>
+                <li class="<?php if($pageId == "program"){ ?>active<?php } ?>"><a href="<?php $REQUEST_URI ?>program">Πρόγραμμα</a></li>
                 <!--<li><a href="#blog">Blog</a></li>-->
-                <li class="<?php if($pageId == "contact"){ ?>active<?php } ?>"><a href="<?php $REQUEST_URI ?>index.php?id=contact">Επικοινωνία</a></li>
+                <li class="<?php if($pageId == "contact"){ ?>active<?php } ?>"><a href="<?php $REQUEST_URI ?>contact">Επικοινωνία</a></li>
                 <!--<li><a href="#bookNow">Book Now</a></li>-->
             </ul>
         </div>
