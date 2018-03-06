1aa845e265f3ed00eba1403fc05a2262bc46e53a
natsaros <natsaros@hotmail.com>
2018-03-06 19:52:13 +0200

    String properties added/Minor UI fixes

--- php/client/home.php
+++ php/client/home.php
@@ -33,7 +33,7 @@
             <div class="col-sm-12">
                 <div class="textHolder">
                     <div class="genColor textHolderInside">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br/><br/>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        <?php getLocalizedText("site_info")?>
                     </div>
                 </div>
             </div>
@@ -69,7 +69,7 @@
             <div class="parallaxImage">
                 <div class="motoCreation img-circle">
                     <div class="motoContainer">
-                        <?php echo getLocalizedText("creation_moto");?>
+                        <?php echo getLocalizedText("parallax_quote");?>
                     </div>
                 </div>
             </div>
