66df8bd729486bec3c9fd14002a17e2cd0324644
natsaros <natsaros@hotmail.com>
2018-03-06 12:59:26 +0200

    Minor UI Fix

--- php/admin/adminHeader.php
+++ php/admin/adminHeader.php
@@ -1,5 +1,5 @@
 <head>
-    <title>Sellinofos - <?php echo ucfirst(preg_replace( '/([a-z0-9])([A-Z])/', "$1 $2", ADMIN_PAGE_ID )); ?></title>
+    <title>Sellinofos</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
--- php/client/maintenancePage.php
+++ php/client/maintenancePage.php
@@ -1,9 +1,8 @@
-<div class="container-fluid" style="padding: 0;">
-    <div class="row row-no-margin row-no-padding">
+<div class="container-fluid" style="padding: 0;height: 100%;">
+    <div class="row row-no-margin row-no-padding" style="height: 100%;">
         <div class="col-sm-12 maintenance-page-message">
             <div class="maintenance-page-image"></div>
-            Stay tuned.<br>
-            We are launching soon!
+            <?php getLocalizedText("maintenance_text");?>
         </div>
     </div>
 </div>
