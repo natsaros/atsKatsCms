6dfed5f78f1b47aec6d33eb26a5130678742ff5c
Nikolaos.ATSAROS <natsaros@intrasoft-intl.com>
2020-04-15 20:04:01 +0300

    CORR_04_20 - add online cources intermediate

---
+++ php/client/online.php
@@ -0,0 +1,25 @@
+<?php
+FormHandler::unsetSessionForm('sendEmailForm');
+?>
+    <div class="container-fluid belowHeader text-center">
+        <div class="row row-no-padding">
+            <div class="col-sm-12">
+                <div class="heroHeader onlineHero">
+                    <div class="headerTitle">
+                        <p>Γυμνάσου απο το σπίτι τωρα!</p>
+                        <div class="titlesBorder"></div>
+                    </div>
+                    <div class="heroSubTitle">Μιλήστε μας για τους στόχους σας...</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <iframe src="https://renkont.com"
+                width="98%" height="1200"
+                style="margin-top: 20px"
+                allowfullscreen="allowfullscreen"
+                frameBorder="0"
+                scrolling="no"></iframe>
+    </div>
